46fb4069b6d12b0891e9a129e8f39ff7344cd186
mugasparetto <mu.gasparetto@hotmail.com>
2021-03-21 15:22:51 -0300

    :sparkles: challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acc.income += transaction.value;
+            break;
+          case 'outcome':
+            acc.outcome += transaction.value;
+          default:
+            break;
+        }
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ type, value, title }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ type, value, title });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
